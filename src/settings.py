7880212ed814e260fb4e05e7db02034a906ce822
Sohailaibrahime66 <124284142+Sohailaibrahime66@users.noreply.github.com>
2025-03-23 05:39:29 +0200

    Fix WhiteNoiseMiddleware import

--- src/settings.py
+++ src/settings.py
@@ -65,7 +65,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddlewarepip'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'src.urls'
